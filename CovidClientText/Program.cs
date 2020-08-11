4775ad70f18da6738288469ff07a76f9b6590687
Carl Stermann-Lücke <carl.stermann-luecke@st.ovgu.de>
2020-08-11 07:55:54 +0200

    Adds non-interactive text client for testing.

---
+++ CovidClientText/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Covid19API1;
+
+namespace CovidClientText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(API.CreateEntry(State.Infected));
+            Console.WriteLine(API.CreateEntry(State.Deceased));
+            Console.WriteLine(API.CreateEntry(State.Infected));
+            //Console.WriteLine("Hello World!");
+            List<Entry> found = API.FindCases(DateTime.Now.Date, State.Infected);
+            //Console.WriteLine(found[0].ToString());
+            foreach(Entry e in found)
+            {
+                Console.WriteLine(e.ToTuple());
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
